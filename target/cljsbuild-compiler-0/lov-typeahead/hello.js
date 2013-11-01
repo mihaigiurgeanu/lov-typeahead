ae3b4c48d7f8dfa6031256c60d5c5d6985ad3ad5
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-11-01 14:38:29 +0200

    Setup bower publication.

---
+++ target/cljsbuild-compiler-0/lov-typeahead/hello.js
@@ -0,0 +1,7 @@
+goog.provide('lov_typeahead.hello');
+goog.require('cljs.core');
+/**
+* I don't do a whole lot.
+*/
+lov_typeahead.hello.foo = (function foo(x){return cljs.core.println.call(null,x,"Hello, World!");
+});
