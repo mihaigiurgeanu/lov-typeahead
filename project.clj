4b17f60113f0fa4c36016b02bd37062b5f77cfb2
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-11-01 12:42:41 +0200

    Binding of the model with the typeahead control (an input).
    It checks a hardcoded "label" property of the model object.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject lov-typeahead "0.1.0-SNAPSHOT"
+  :description "A list of values AngularJS component based on Twitter's typeahead"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "0.3.4"]]
+  :dependencies [[org.clojure/clojurescript "0.0-1978"]]
+  :cljsbuild {
+    :builds [{
+      :source-paths ["src/cljs"]
+      :compiler {
+        :output-to "target/js/lov-typehead.js"
+        :optimizations :whitespace
+        :pretty-print true}}]})
---
+++ src/cljs/lov_typeahead/directive.cljs
@@ -0,0 +1,10 @@
+(ns lov-typeahead.directive)
+
+(def theModule (.module js/angular "lovTypeaheadDirecive" (clj->js [])))
+(.directive theModule
+  (fn []
+    (clj->js
+      {:link (fn [scope element attrs] 
+               (let [label "label"]
+                 (.$watch scope (.-ngModel attrs)
+                   (fn [value] (get value label)))))})))
