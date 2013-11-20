2c683ac77bba085495a4aafe98417e221521b13a
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-11-20 13:49:39 +0200

    Changed to cljsbuild 1.0.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cljsbuild "0.3.4"]]
+  :plugins [[lein-cljsbuild "1.0.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojurescript "0.0-1978"]]
   :cljsbuild {
     :builds [{
